73a8fb3916ea46e9b4d650a420c138c56cbca747
Armen <armentovmasyan02@gmail.com>
2020-06-09 21:36:40 +0300

    Добавил корзину для страницы заказа

--- src/app/order-registration/order-cart/order-cart.component.ts
+++ src/app/order-registration/order-cart/order-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CartItem} from '../../сlasses/cartItem'
 
 @Component({
   selector: 'app-order-cart',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderCartComponent implements OnInit {
 
+  cartItems:CartItem[];
   constructor() { }
 
   ngOnInit() {
+    this.cartItems=JSON.parse(localStorage.getItem ("itemsInCart"));
   }
 
+
+  totalPriceForItem(item)
+  {
+    console.log(item)
+    let cartItem:CartItem=this.cartItems.find(i=>i.item.id==item.item.id);
+    return cartItem.count * cartItem.item.price
+  }
+
+  totalPrice()
+  {
+    let total=0;
+    this.cartItems.forEach(i=>total+=i.count * i.item.price)
+    return total
+  }
 }

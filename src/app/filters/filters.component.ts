151feca6f084144412563664ef97e2ee475f10c3
Armen <armentovmasyan02@gmail.com>
2020-06-09 21:37:20 +0300

    Добавил новые фильтры

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class FiltersComponent implements OnInit {
 
   panelOpenState = false;
+  S = false;
+  M = false;
+  XXL=false;
+  XXXL=true;
+  red=false;
+  public black=true;
+  white=false;
+  yellow=false;
   constructor() { }
 
   ngOnInit() {

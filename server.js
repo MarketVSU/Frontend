200aa4e0cce602615216dc622422fd4a665c7f4d
Armen <armentovmasyan02@gmail.com>
2020-06-09 23:52:33 +0300

    обновил server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Store'));
+app.use(express.static(__dirname + '/dist/market'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Store/index.html'));
+res.sendFile(path.join(__dirname+'/dist/market/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

d02867d7935697700de927f283b55b37a70ff69c
Armen <armentovmasyan02@gmail.com>
2020-06-10 17:29:24 +0300

    Добавил страницу OrderComplete

---
+++ src/app/order-complete-page/order-complete-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-complete-page',
+  templateUrl: './order-complete-page.component.html',
+  styleUrls: ['./order-complete-page.component.scss']
+})
+export class OrderCompletePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
